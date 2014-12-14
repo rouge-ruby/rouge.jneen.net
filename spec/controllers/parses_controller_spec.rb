a4d808f692040cdc588a802170afdc7e49ecbf9e
Edward Loveall <edward@edwardloveall.com>
2014-12-14 17:26:38 -0500

    Fix failing tests

--- spec/controllers/parses_controller_spec.rb
+++ spec/controllers/parses_controller_spec.rb
@@ -2,14 +2,31 @@ require 'rails_helper'
 
 describe ParsesController do
   describe '#create' do
-    it 'returns a html tagged version of the input code' do
-      ruby_code = load_fixture 'code.rb'
+    it 'assigns a html tagged version of the input code to @parse' do
       tagged_ruby = load_fixture 'ruby_code.html'
-      params = { language: :ruby, source: ruby_code }
 
-      post :create, parse: params
+      post_parse
 
-      expect(response.body).to eq(tagged_ruby)
+      expect(assigns[:parse]).to eq(tagged_ruby)
     end
+
+    it 'renders the :create template' do
+      post_parse
+
+      expect(response).to render_template(:create)
+    end
+
+    it 'renders with no layout' do
+      post_parse
+
+      expect(response).not_to render_template(layout: 'application')
+    end
+  end
+
+  def post_parse
+    ruby_code = load_fixture 'code.rb'
+    params = { language: :ruby, source: ruby_code }
+
+    post :create, parse: params
   end
 end
